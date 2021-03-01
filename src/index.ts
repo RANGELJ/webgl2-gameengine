a05b0da0c6ec0c821579bd8991efda3df2861942
Jorge Rangel <jorgerangel@Jorges-Mac-mini.local>
2021-03-01 07:46:17 -0600

    Initial ts setup

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+const component = () => {
+    const element = document.createElement('div')
+    element.innerHTML = 'Hello webpack 2'
+    return element
+}
+
+document.body.appendChild(component())
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+module.exports = {
+    entry: './src/index.ts',
+    mode: 'production',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'Webgl2 engine',
+        }),
+    ],
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 8080,
+    },
+}
